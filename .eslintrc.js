ebeae6fd3f93f252a5f3518fc58a2cde3b86b4dd
matheus costa <matheuscosta.tech@gmail.com>
2022-05-24 22:14:43 -0300

    update eslint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,24 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true
-	},
-	extends: ['eslint:recommended', 'plugin:prettier/recommended'],
-	parserOptions: {
-		ecmaFeatures: {
-			jsx: true
-		},
-		ecmaVersion: 'latest',
-		sourceType: 'module'
-	},
-	plugins: ['react'],
-	rules: {
-		'linebreak-style': ['error', 'unix'],
-		quotes: ['error', 'single'],
-		semi: ['error', 'always'],
-		'react/prop-types': 'off',
-		'react/jsx-uses-react': 'off',
-		'react/react-in-jsx-scope': 'off',
-		'react/self-closing-comp': 'warn'
-	}
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  rules: {
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'react/prop-types': 'off',
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/self-closing-comp': 'warn'
+  }
 };
